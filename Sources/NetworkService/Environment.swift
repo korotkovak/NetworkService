d9ed769c7f2f30b2d3b29a29c106399a8144a3c7
korotkovak <kristinakorotkova059@gmail.com>
2023-04-11 19:24:53 +0800

    Environmet now class

--- Sources/NetworkService/Environment.swift
+++ Sources/NetworkService/Environment.swift
@@ -7,11 +7,23 @@
 
 import Foundation
 
-public class Environment {
-    static public var scheme = "https"
-    static public var host = "gateway.marvel.com"
+//public class Environment {
+//    static public var scheme = "https"
+//    static public var host = "gateway.marvel.com"
+//
+//    static var baseURL: String {
+//        "\(Environment.scheme)://\(Environment.host)"
+//    }
+//}
 
-    static var baseURL: String {
-        "\(Environment.scheme)://\(Environment.host)"
+public protocol Environment: AnyObject {
+    var scheme: String { get }
+    var host: String { get }
+    var baseURL: String { get }
+}
+
+public extension Environment {
+    var baseURL: String {
+        "\(scheme)://\(host)"
     }
 }
--- Tests/NetworkServiceTests/NetworkServiceTests.swift
+++ Tests/NetworkServiceTests/NetworkServiceTests.swift
@@ -6,6 +6,5 @@ final class NetworkServiceTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(NetworkService().text, "Hello, World!")
     }
 }
